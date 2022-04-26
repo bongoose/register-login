e868f419713bc1aba399b0bbc14949e3bce62ae4
bongoose <hduchung88.mn@gmail.com>
2022-04-26 10:57:16 +0700

    edit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
@@ -6,16 +6,18 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
+app.use("/styles", express.static(__dirname + "/styles"));
 app.use(expressLayouts);
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});``
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+``
 const db = mongoose.connection;
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to MongoDB'));
@@ -23,4 +25,3 @@ db.once('open', () => console.log('Connected to MongoDB'));
 app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000);
-
